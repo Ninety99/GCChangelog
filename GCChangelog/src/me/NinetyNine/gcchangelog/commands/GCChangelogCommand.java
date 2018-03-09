8d796346e6e1d75308833fc124ba2c653532ca0c
BuildTools <unconfigured@null.spigotmc.org>
2018-03-09 22:45:46 +0800

    Last commit for the day.

--- GCChangelog/src/me/NinetyNine/gcchangelog/commands/GCChangelogCommand.java
+++ GCChangelog/src/me/NinetyNine/gcchangelog/commands/GCChangelogCommand.java
@@ -1,5 +1,6 @@
 package me.NinetyNine.gcchangelog.commands;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 import org.bukkit.Material;
@@ -10,30 +11,38 @@ import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_8_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+
+import me.NinetyNine.gcchangelog.GCChangelog;
 
 public class GCChangelogCommand implements Listener, CommandExecutor {
 
 	private HashSet<String> msg = new HashSet<String>();
-	// private List<String> mesg;
-
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
 		String message = "";
+		
+        for (int i = 1; i < args.length; i++) {
+            message += args[i] + " ";
+        }
+        message = message.trim();
+		
+		String down = "\n";
 
 		Player player = (Player) sender;
 		ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
 		BookMeta bookmeta = (BookMeta) book.getItemMeta();
 		bookmeta.setTitle("§6Guild§7Craft §0Changelog");
 		bookmeta.setAuthor("§caXed");
-		bookmeta.addPage("§2✔​ §fHey!");
+		bookmeta.addPage("§2✔​ §f message" + msg);
 		book.setItemMeta(bookmeta);
-		// bookmeta.addPage(message);
 
 		if (cmd.getName().equalsIgnoreCase("changelog")) {
 			if (player.hasPermission("changelog.open")) {
-				player.getInventory().addItem(book);
+				player.getInventory().setItem(0, book);
 
 				((CraftPlayer) player).getHandle().openBook(CraftItemStack.asNMSCopy(book));
 			} else {
@@ -64,24 +73,28 @@ public class GCChangelogCommand implements Listener, CommandExecutor {
 					}
 				}
 
-				for (int i = 1; i < args.length; i++) {
-					message += args[i] + " ";
-				}
-				message = message.trim();
-
 				if (args[0].equalsIgnoreCase("undo")) {
 					player.sendMessage("§8[§6Guild§7Craft§8] §aSuccesfully undo!");
 					msg.remove(message);
 					return true;
+				}
+				
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (args.length == 1) {
+						new GCChangelog().reloadConfig();
+						
+						player.sendMessage("§8[§6Guild§7Craft§8] §6Reloaded!");
+					}
 				}
 
 				if (args[1].equalsIgnoreCase("fixed")) {
 					if (args.length == 1) {
 						player.sendMessage("§8[§6Guild§7Craft§8] §cUsage: /gcchangelog add fixed <message>");
 					}
+					
 					bookmeta.setTitle("§6Guild§7Craft §0Changelog");
 					bookmeta.setAuthor("§caXed");
-					bookmeta.addPage("§2✔​ §f" + message);
+					bookmeta.addPage(down + "§2✔​  §l§0" + message);
 					book.setItemMeta(bookmeta);
 					player.sendMessage("§8[§6Guild§7Craft§8] §2Added!");
 
@@ -113,7 +126,7 @@ public class GCChangelogCommand implements Listener, CommandExecutor {
 				}
 
 				else if (args[1].equalsIgnoreCase("page")) {
-					if (bookmeta.getPageCount() < 6) {
+					if (bookmeta.getPageCount() < (new GCChangelog().getConfig().getInt("maxPages"))) {
 						bookmeta.addPage("§8[§6Guild§7Craft§8] §2New page!");
 
 						player.sendMessage("§8[§6Guild§7Craft§8] §7+1 page");
