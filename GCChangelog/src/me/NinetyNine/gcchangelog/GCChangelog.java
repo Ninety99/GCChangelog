a90fb0608a0ac1f11f7a2f1b9bfbf09a7875cf03
BuildTools <unconfigured@null.spigotmc.org>
2018-03-09 18:53:38 +0800

    More changes.

--- GCChangelog/src/me/NinetyNine/gcchangelog/GCChangelog.java
+++ GCChangelog/src/me/NinetyNine/gcchangelog/GCChangelog.java
@@ -3,11 +3,19 @@ package me.NinetyNine.gcchangelog;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.NinetyNine.gcchangelog.commands.GCChangelogCommand;
+
 public class GCChangelog extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new GCChangelogHandler(), this);
+		getCommand("changelog").setExecutor(new GCChangelogCommand());
+		getCommand("gcchangelog").setExecutor(new GCChangelogCommand());
+		
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		reloadConfig();
 	}
 	
 	@Override
--- GCChangelog/src/me/NinetyNine/gcchangelog/commands/GCChangelogCommand.java
+++ GCChangelog/src/me/NinetyNine/gcchangelog/commands/GCChangelogCommand.java
@@ -2,6 +2,7 @@ package me.NinetyNine.gcchangelog.commands;
 
 import org.bukkit.Material;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_8_R1.inventory.CraftItemStack;
@@ -10,33 +11,38 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
-public class GCChangelogCommand implements Listener {
+public class GCChangelogCommand implements Listener, CommandExecutor {
 
-	public boolean onCommand(CommandSender sender, Command cmd, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
 		Player player = (Player) sender;
 		ItemStack book = new ItemStack(Material.WRITTEN_BOOK, 1);
+		String entered = "";
 
 		if (cmd.getName().equalsIgnoreCase("changelog")) {
 			if (player.hasPermission("changelog.open")) {
 				BookMeta bookmeta = (BookMeta) book.getItemMeta();
 				bookmeta.setTitle("§dGuild" + "§7Craft §fChangelog");
 				bookmeta.setAuthor("§cNinetyNine");
-				bookmeta.addPage("§l§08th of March\n" + "§2✔ §0Fixed issue with boosters.\n​ "
-						+ "§2✔ §0Fixed issue with kits.\n\n" + "§l§07th of March\n"
-						+ "§2✔ §0Fixed bug with items from supply drops.\n"
-						+ "§2✔ §0Fixed issue with items from crates.");
+				bookmeta.addPage(entered);
 
 				book.setItemMeta(bookmeta);
-				
+
 				((CraftPlayer) player).getHandle().openBook(CraftItemStack.asNMSCopy(book));
 			} else {
 				player.sendMessage("§[§dGuild§7Craft§8] §cYou do not have permissions!");
 				return false;
 			}
 		}
-
+		
+		if (cmd.getName().equalsIgnoreCase("gcchangelog")) {
+			if (player.hasPermission("changelog.admin")) {
+				player.sendMessage("hi");
+			} else {
+				player.sendMessage("§[§dGuild§7Craft§8] §cYou do not have permissions!");
+				return false;
+			}
+		}
 		return true;
 	}
-
 }
