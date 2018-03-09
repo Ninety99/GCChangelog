e5867912652fa3f51b402ad9200be309f6b99cd7
BuildTools <unconfigured@null.spigotmc.org>
2018-03-09 20:29:35 +0800

    Added and edited a lot of code.

--- GCChangelog/src/me/NinetyNine/gcchangelog/commands/GCChangelogCommand.java
+++ GCChangelog/src/me/NinetyNine/gcchangelog/commands/GCChangelogCommand.java
@@ -1,5 +1,7 @@
 package me.NinetyNine.gcchangelog.commands;
 
+import java.util.HashSet;
+
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,37 +12,103 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.plugin.Plugin;
+
+import me.NinetyNine.gcchangelog.GCChangelog;
 
 public class GCChangelogCommand implements Listener, CommandExecutor {
+
+	private HashSet<String> msg = new HashSet<String>();
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
 		Player player = (Player) sender;
 		ItemStack book = new ItemStack(Material.WRITTEN_BOOK, 1);
-		String entered = "";
+		BookMeta bookmeta = (BookMeta) book.getItemMeta();
+
+		String message = "";
+		for (int i = 1; i < args.length; i++) {
+			message += args[i] + " ";
+		}
+		message = message.trim();
 
 		if (cmd.getName().equalsIgnoreCase("changelog")) {
 			if (player.hasPermission("changelog.open")) {
-				BookMeta bookmeta = (BookMeta) book.getItemMeta();
-				bookmeta.setTitle("§dGuild" + "§7Craft §fChangelog");
-				bookmeta.setAuthor("§cNinetyNine");
-				bookmeta.addPage(entered);
+				BookMeta bookmeta1 = (BookMeta) book.getItemMeta();
+				bookmeta1.setTitle("§dGuild§7Craft §fChangelog");
+				bookmeta1.setAuthor("§4aXed");
 
 				book.setItemMeta(bookmeta);
 
 				((CraftPlayer) player).getHandle().openBook(CraftItemStack.asNMSCopy(book));
 			} else {
-				player.sendMessage("§[§dGuild§7Craft§8] §cYou do not have permissions!");
+				player.sendMessage("§8[§6Guild§7Craft§8] §cYou do not have permissions!");
 				return false;
 			}
 		}
-		
+
 		if (cmd.getName().equalsIgnoreCase("gcchangelog")) {
-			if (player.hasPermission("changelog.admin")) {
-				player.sendMessage("hi");
-			} else {
-				player.sendMessage("§[§dGuild§7Craft§8] §cYou do not have permissions!");
-				return false;
+			if (args.length == 0) {
+				if (player.hasPermission("changelog.admin")) {
+					player.sendMessage(
+							"§8[§6Guild§7Craft§8] §cUsage: /gcchangelog add <type> <message> or /gcchangelog remove <number>");
+					return true;
+				} else {
+					player.sendMessage("§8[§6Guild§7Craft§8] §cYou do not have permissions!");
+					return false;
+				}
+			}
+
+			if (args[0].equalsIgnoreCase("add")) {
+				if (args.length == 1) {
+					player.sendMessage(
+							"§8[§6Guild§7Craft§8] §cUsage: /gcchangelog add <type> <message> or /gcchangelog remove <number>");
+					return true;
+				}
+			}
+			
+			if (args[0].equalsIgnoreCase("undo")) {
+				msg.remove(message);
+				return true;
+			}
+
+			if (args[1].equalsIgnoreCase("fixed")) {
+				bookmeta.addPage("§2✔​ §f" + message);
+				book.setItemMeta(bookmeta);
+				player.sendMessage("§8[§6Guild§7Craft§8] §2Added!");
+				
+				msg.add(message);
+			}
+
+			else if (args[1].equalsIgnoreCase("removed")) {
+				bookmeta.addPage("§c╳ §f" + message);
+				book.setItemMeta(bookmeta);
+				player.sendMessage("§8[§6Guild§7Craft§8] §2Added!");
+				
+				msg.add(message);
+			}
+
+			else if (args[1].equalsIgnoreCase("changed")) {
+				bookmeta.addPage("§f▶ " + message);
+				book.setItemMeta(bookmeta);
+				player.sendMessage("§8[§6Guild§7Craft§8] §2Added!");
+				
+				msg.add(message);
+			}
+
+			else if (args[1].equalsIgnoreCase("page")) {
+				if (bookmeta.getPageCount() < ((Plugin) new GCChangelog()).getConfig().getInt("maxPage")) {
+					bookmeta.addPage();
+				} else {
+					player.sendMessage("§8[§6Guild§7Craft§8] §4Reached the maximum level of pages!");
+					return false;
+				}
+				
+				msg.add(message);
+			}
+
+			else {
+				player.sendMessage("§[§dGuild§7Craft§8] §cInvalid operation.");
 			}
 		}
 		return true;
